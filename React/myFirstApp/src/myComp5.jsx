cd454ffe17f262a2eb7fb0164f162e7875ad15a2
abhayraj81 <abhay.ark12@gmail.com>
2026-08-12 15:25:38 +0530

    comp 5 parameterized function calling with event handling

--- React/myFirstApp/src/myComp5.jsx
+++ React/myFirstApp/src/myComp5.jsx
@@ -1,10 +1,32 @@
 function Comp5(){
-    
+
+    let myCss = {
+        backgroundColor:"navy",
+        color:"skyblue",
+        textAlign:"center",
+        lineHeight:"100px",
+        boxShadow:"0 5px 8px grey"
+    }
+
+    function myfun(){
+        alert("Helooooooo........")
+    }
+
+    function myfun2(name){
+        alert("Helooooooo........" + name)
+    }
+
     
 
     return(
-        <>
-            
+        <>  
+            <h1 style={myCss} align="center">Component 5</h1>
+            <button onClick={myfun} className="btn btn-warning">
+                Click Me !
+            </button>
+            <button onClick={()=>{myfun2("Abhay Raj")}} className="btn btn-warning">
+                Tell me Name 
+            </button>
         </>
     )
 }
