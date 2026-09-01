7ecb6f4074be21afea7878c410c13a526e4f288f
abhayraj81 <abhay.ark12@gmail.com>
2026-09-01 23:43:45 +0530

    Added : controller : uploadProfilePicController

--- Express/controllers/uploadProfileController.js
+++ Express/controllers/uploadProfileController.js
@@ -1,1 +1,29 @@
-const { sendError } = require("../utils/responseHelper")
+const MESSAGES = require("../constants/messages");
+const STATUS_CODES = require("../constants/statusCodes");
+const UserModel = require("../models/userModel");
+const { sendError, sendSuccess } = require("../utils/responseHelpers");
+
+const uploadProfilePicController = async (req, res, next) => {
+  try {    
+
+    if (!req.file)
+      return sendError(
+        res,
+        STATUS_CODES.BAD_REQUEST,
+        MESSAGES.COMMON.UPLOAD_ERROR,
+      );
+
+    const user = UserModel.findById(req.user.id);
+    if (!user)
+      return sendError(res, STATUS_CODES.NOT_FOUND, MESSAGES.USER.NOT_FOUND);
+
+    user.profilePic = req.file.fileName;
+    await user.save();
+
+    return sendSuccess(res, STATUS_CODES.OK, MESSAGES.USER.UPDATED);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = uploadProfilePicController;
